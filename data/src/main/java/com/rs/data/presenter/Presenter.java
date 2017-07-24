b71ccc740518add4d23160c925b00c7a6bce32c3
zhaolong.jiang <zhaolong.jiang@chinaredstar.com>
2017-07-24 16:49:37 +0800

    fix the autowried

--- data/src/main/java/com/rs/data/presenter/Presenter.java
+++ data/src/main/java/com/rs/data/presenter/Presenter.java
@@ -1,9 +1,11 @@
 package com.rs.data.presenter;
 
+import com.alibaba.android.arouter.facade.template.IProvider;
+
 /**
  * Created by jiangdylan on 2017/7/20.
  */
 
-public interface Presenter {
+public interface Presenter extends IProvider {
     void hi(String text);
 }
--- module-java/src/main/java/com/alibaba/android/arouter/demo/module1/TestModuleActivity.java
+++ module-java/src/main/java/com/alibaba/android/arouter/demo/module1/TestModuleActivity.java
@@ -3,14 +3,20 @@ package com.alibaba.android.arouter.demo.module1;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.rs.data.presenter.Presenter;
 
 @Route(path = "/module/1")
 public class TestModuleActivity extends Activity {
-
+    @Autowired
+    Presenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_module);
+        ARouter.getInstance().inject(this);
+        presenter.hi("from the bundle");
     }
 }
